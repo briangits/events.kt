aafc173c3fd5034a1d1d0a29f1c62363faf5d85d
briangits <briangits@proton.me>
2026-09-13 12:47:05 +0300

    docs(serialization/core): add KDocs for Serializer

--- serialization/core/src/commonMain/kotlin/io/github/briangits/events/integration/serialization/Serializer.kt
+++ serialization/core/src/commonMain/kotlin/io/github/briangits/events/integration/serialization/Serializer.kt
@@ -1,9 +1,32 @@
 package io.github.briangits.events.integration.serialization
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.serializer
 
+/**
+ * An interface for serializing and deserializing data to and from [ByteArray].
+ *
+ * Implementations of this interface define how objects are converted to bytes
+ * and vice versa, typically using a specific format like JSON or Protobuf.
+ */
 interface Serializer {
+    /**
+     * Serializes the given [value] into a [ByteArray] using the provided [serializer].
+     *
+     * @param T The type of the value to serialize.
+     * @param value The object to serialize.
+     * @param serializer The [KSerializer] to use for serialization.
+     * @return The serialized [ByteArray].
+     */
     fun <T> serialize(value: T, serializer: KSerializer<T>): ByteArray
-    
+
+    /**
+     * Deserializes the given [bytes] into an object of type [T] using the provided [serializer].
+     *
+     * @param T The type of the object to deserialize.
+     * @param bytes The raw [ByteArray] to deserialize.
+     * @param serializer The [KSerializer] to use for deserialization.
+     * @return The deserialized object of type [T].
+     */
     fun <T> deserialize(bytes: ByteArray, serializer: KSerializer<T>): T
 }
